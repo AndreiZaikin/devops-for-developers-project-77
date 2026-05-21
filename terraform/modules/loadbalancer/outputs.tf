ee478419edc69bb3ad08057f1d2f7a6dd1dafad2
Andrei Zaikin <andrei@my-kin.ru>
2026-05-21 20:56:14 +0300

    fixed outputs

--- terraform/modules/loadbalancer/outputs.tf
+++ terraform/modules/loadbalancer/outputs.tf
@@ -1,3 +1,3 @@
 output "external_ip" {
-  value = yandex_alb_load_balancer.redmine-alb.listener[*].endpoint[*].address[*].external_ipv4_address[*].address
+  value = yandex_alb_load_balancer.redmine-alb.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
 }
